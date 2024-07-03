4b69a88ff38bd5ab582fdd78a3b15b8df706025f
bishnu <bishnu@gmail.com>
2024-07-03 21:15:05 +0530

    messagemodel rename

--- doyo_backend/src/model/messagemodel.js
+++ doyo_backend/src/model/messagemodel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   content: { type: String, required: true },
   bookingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
   createdAt: { type: Date, default: Date.now },
