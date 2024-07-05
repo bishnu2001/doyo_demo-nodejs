b8f392a6b36c563d26afbefb2ba08b9287211f31
bishnu <bishnu@gmail.com>
2024-07-06 00:05:50 +0530

    socket.io implement

--- doyo_backend/src/controller/message.controller.js
+++ doyo_backend/src/controller/message.controller.js
@@ -1,10 +1,37 @@
-const {sendMessage} =require("../business-logic/message.business-logic");
-const { Conflict ,NotFound,Unauthorized} = require("http-errors");
 
-const sendmessage=async()=>{
+const { sendMessage,getMessage} = require("../business-logic/message.business-logic");
+const { Conflict, NotFound, Unauthorized } = require("http-errors");
 
+const sendmessage = async (req, res, next) => {
+  try {
+    const senderId=req.userId;
+    const { userid } = req.params;
+    const{content,bookingId}=req.body;
+    const receiverId=userid
+    const postmessage = await sendMessage({ senderId, receiverId, content ,bookingId});
+    res.json({
+      success: true,
+      message: "message create successfull",
+      data: postmessage,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const getmessage=async(req,res,next)=>{
+    try {
+        const{userid}=req.params;
+        const message=await getMessage({userid});
+        res.json({
+            success:true,
+            message:"all messages",
+            data:message
+        })
+    } catch (error) {
+        next(error)
+    }
 }
-
-module.exports.createMessage={
-    sendmessage
-}
+module.exports.createMessage = {
+  sendmessage,
+  getmessage
+};
--- doyo_backend/src/routes/message.routes.js
+++ doyo_backend/src/routes/message.routes.js
@@ -1,9 +1,10 @@
+
 const express=require('express');
 const{createMessage}=require('../controller/message.controller');
 const {Users}=require('../validation/users.validation');
 const{authentication}=require('../middleware/authenticationToken.middleware')
 const router=express.Router();
-router.post('/postmessage',authentication,createMessage.sendmessage)
-// router.post('/getmessage',Users.login,Usercontroller.userSignin);
+router.post('/postmessage/:userid',authentication,createMessage.sendmessage)
+router.get('/getmessage/:userid',authentication,createMessage.getmessage);
 
   module.exports = router;
